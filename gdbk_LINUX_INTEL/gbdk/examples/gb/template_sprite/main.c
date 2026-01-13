054c2e60fef355293bbe22f84b4c7b73bea42e93
bensamazingsoft <ben.mouginot@gmail.com>
2026-01-13 13:43:55 +0000

    sprite build tut build with move

--- gdbk_LINUX_INTEL/gbdk/examples/gb/template_sprite/main.c
+++ gdbk_LINUX_INTEL/gbdk/examples/gb/template_sprite/main.c
@@ -7,16 +7,80 @@ void main(void)
     DISPLAY_ON;
     SHOW_SPRITES;
 
-    set_sprite_data(0,1,new_tileset);
+    set_sprite_data(0, 1, new_tileset);
 
-    set_sprite_tile(0,0);
-    move_sprite(0,84,88);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 84, 88);
+
+    uint8_t spriteX, spriteY;
+    int8_t velocityX, velocityY;
+
+    // Set the sprite's default position
+    spriteX = 80;
+    spriteY = 72;
+
+    // Set our default velocity to be down and to the right
+    velocityX = 1;
+    velocityY = 1;
 
     // Loop forever
     while (1)
     {
 
         // Game main loop processing goes here
+        // Apply our velocity
+        spriteX += velocityX;
+        spriteY += velocityY;
+
+        // When we get too far to the right while moving to the right
+        if (spriteX > 156 && velocityX > 0)
+        {
+
+            // Switch directions for our x velocity
+            velocityX = -velocityX;
+
+            // Clamp our x position back down to 156 so we don't go past the edge
+            spriteX = 156;
+        }
+
+        // When we get too far down on the screen while moving downards
+        if (spriteY > 140 && velocityY > 0)
+        {
+
+            // Switch directions for our y velocity
+            velocityY = -velocityY;
+
+            // Clamp our y position back down to 140 so we don't go past the edge
+            spriteY = 140;
+        }
+
+        // When we get too far to the left while moving left
+        if (spriteX < 4 && velocityX < 0)
+        {
+
+            // Switch directions for our x velocity
+            velocityX = -velocityX;
+
+            // Clamp our x position back up to 4 so we don't go past the edge
+            spriteX = 4;
+        }
+
+        // When we get too far towards the top of the screen while moving upwards
+        if (spriteY < 4 && velocityY < 0)
+        {
+
+            // Switch directions for our y velocity
+            velocityY = -velocityY;
+
+            // Clamp our y position back up to 4 so we don't go past the edge
+            spriteY = 4;
+        }
+
+        // Position the first sprite at our spriteX and spriteY
+        // All sprites are render 8 pixels to the left of their x position and 16 pixels ABOVE their actual y position
+        // This means an object rendered at 0,0 will not be visible
+        // x+5 and y+12 will center the 8x8 tile at our x and y position
+        move_sprite(0, spriteX + 4, spriteY + 12);
 
         // Done processing, yield CPU and wait for start of next frame
         vsync();
