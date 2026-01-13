36691c15e29ec817d1808995a35433cee3454367
bensamazingsoft <ben.mouginot@gmail.com>
2026-01-13 10:50:12 +0000

    sprite tut build

--- gdbk_LINUX_INTEL/gbdk/examples/gb/template_sprite/main.c
+++ gdbk_LINUX_INTEL/gbdk/examples/gb/template_sprite/main.c
@@ -1,17 +1,24 @@
 #include <gb/gb.h>
 #include <stdint.h>
-
+#include "New_tileset.h"
 
 void main(void)
 {
+    DISPLAY_ON;
+    SHOW_SPRITES;
+
+    set_sprite_data(0,1,new_tileset);
+
+    set_sprite_tile(0,0);
+    move_sprite(0,84,88);
+
     // Loop forever
-    while(1) {
+    while (1)
+    {
 
+        // Game main loop processing goes here
 
-		// Game main loop processing goes here
-
-
-		// Done processing, yield CPU and wait for start of next frame
+        // Done processing, yield CPU and wait for start of next frame
         vsync();
     }
 }
